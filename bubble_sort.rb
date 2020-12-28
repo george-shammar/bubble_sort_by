9e421f13f51791224733d06969113c1f1897809c
George <george.gbenle@outlook.com>
2020-12-28 08:11:52 -0500

    Define left and right variable
    Left and right variable are the two items in an array to be compared

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,11 @@
 def bubble_sort_by(arr)
     arr.length.times do
-        
+        arr.each_with_index do | x, i|
+            left = arr[i]
+            right = arr[i + 1]
+            
+                
+        end
     end
 end
     
