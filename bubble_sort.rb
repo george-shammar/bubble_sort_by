d241b8c4554659bf58b71298865b785f29627182
George <george.gbenle@outlook.com>
2020-12-28 18:29:17 -0500

    create code block for bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,16 @@
 def bubble_sort(arr)
   arr.length.times do
     arr.each_with_index do |_x, i|
+      left = arr[i]
+      if arr[-1] != left && (arr[i] - arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      end
+    end
+  end
+  arr
+end
 
+puts bubble_sort([4,3,78,2,0,2])
 
 
 def bubble_sort_by(arr)
