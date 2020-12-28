d97edac23102402a300d18ac9246b85565a023b9
George <george.gbenle@outlook.com>
2020-12-28 11:07:36 -0500

    Fix indentation errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,10 +5,10 @@ def bubble_sort_by(arr)
           if (arr[-1] != left) then
               if((arr[i].length) - (arr[i + 1].length) > 0) then
                   arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                end
-            end     
-        end
-    end
+              end
+          end
+      end
+  end
   arr
 end
 puts bubble_sort_by(['hi', 'hello', 'hey'])
