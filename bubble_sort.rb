a5d53dd77936ade6d7de3d8cd0e8b0b11556a96f
George <george.gbenle@outlook.com>
2020-12-28 11:25:30 -0500

    Fix inconsistent indentation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,9 @@
 def bubble_sort_by(arr)
   arr.length.times do
     arr.each_with_index do |_x, i|
-        left = arr[i]
+      left = arr[i]
           if (arr[-1] != left) && (arr[i].length - arr[i + 1].length).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
           end
     end
   end
