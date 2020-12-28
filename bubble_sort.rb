d6101324040a82f57bd32ca921d928b7d56e9584
George <george.gbenle@outlook.com>
2020-12-28 10:45:16 -0500

    Removed useless variable assignment
    right = arr[i + 1]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,7 @@
 def bubble_sort_by(arr)
   arr.length.times do
       arr.each_with_index do |_x, i|
-          left = arr[i]
-          right = arr[i + 1]
+            left = arr[i]
           if (arr[-1] != left)
               if((arr[i].length) - (arr[i + 1].length) > 0) then
                   arr[i], arr[i + 1] = arr[i + 1], arr[i]
