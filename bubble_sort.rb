e7748a41690b8874c000988af4fad41858ab894b
George <george.gbenle@outlook.com>
2020-12-28 12:03:34 -0500

    Fix indentation

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,14 @@
 def bubble_sort_by(arr)
-arr.length.times do
-arr.each_with_index do |_x, i|
-left = arr[i]
-  if arr[-1] != left
-    if (arr[i].length - arr[i + 1].length).positive?
-      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+  arr.length.times do
+    arr.each_with_index do |_x, i|
+      left = arr[i]
+        if arr[-1] != left
+          if (arr[i].length - arr[i + 1].length).positive?
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          end
+        end
     end
   end
-end
-end
-arr
+  arr
 end
 puts bubble_sort_by(%w[hi hello hey])
