444cb5ced204bbd952301e0b8fdda92ff800d728
George <george.gbenle@outlook.com>
2020-12-28 10:49:21 -0500

    fix indentation errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,8 @@
 def bubble_sort_by(arr)
   arr.length.times do
       arr.each_with_index do |_x, i|
-            left = arr[i]
-          if (arr[-1] != left)
+          left = arr[i]
+          if (arr[-1] != left) then
               if((arr[i].length) - (arr[i + 1].length) > 0) then
                   arr[i], arr[i + 1] = arr[i + 1], arr[i]
                 end
