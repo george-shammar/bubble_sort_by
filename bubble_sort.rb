e59e981092ee775085f3cc760c6cedd549685b0d
George <george.gbenle@outlook.com>
2020-12-29 18:02:08 -0500

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,8 +3,10 @@ def bubble_sort(arr)
   arr.length.times do
     arr.each_with_index do |_x, i|
       left = arr[i]
-      if arr[-1] != left && (arr[i] - arr[i + 1]).positive?
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      if arr[-1] != left 
+        if (arr[i] - arr[i + 1]).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        end
       end
     end
   end
@@ -17,8 +19,10 @@ def bubble_sort_by(arr)
   arr.length.times do
     arr.each_with_index do |_x, i|
       left = arr[i]
-      if arr[-1] != left && (arr[i].length - arr[i + 1].length).positive?
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      if arr[-1] != left 
+        if (arr[i].length - arr[i + 1].length).positive?
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        end
       end
     end
   end
