2e9cec57790a3df25177dde373f515cbb9771933
George <george.gbenle@outlook.com>
2020-12-30 16:33:39 -0500

    fix linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,15 +19,13 @@ def bubble_sort_by(arr)
     arr.each_with_index do |_x, i|
       left = arr[i]
       if arr[-1] != left
-        if yield(arr[i].length - arr[i + 1].length).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        yield arr[i], arr[i + 1] = arr[i + 1], arr[i] if (arr[i].length - arr[i + 1].length).positive?
         end
-      end
     end
   end
   arr
 end
 
-p bubble_sort_by(%w[hi hello hey])do |left,right|
+p bubble_sort_by(%w[hi hello hey]) do |left,right|
      left.length - right.length
   end
