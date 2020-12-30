de2e553747c48779dff2d3bddf1551d304cc51ed
George <george.gbenle@outlook.com>
2020-12-30 17:46:14 -0500

    fix identation error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,7 +19,7 @@ def bubble_sort_by(arr)
     arr.each_with_index do |_x, i|
       left = arr[i]
       if arr[-1] != left
-       yield arr[i], arr[i + 1] = arr[i + 1], arr[i] if (arr[i].length - arr[i + 1].length).positive?
+        yield arr[i], arr[i + 1] = arr[i + 1], arr[i] if (arr[i].length - arr[i + 1].length).positive?
       end
     end
   end
