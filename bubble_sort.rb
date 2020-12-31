9e383d8eae6ee16366b64b7d257c6ff4fc4f2929
George <george.gbenle@outlook.com>
2020-12-31 12:19:15 -0500

    delete trailing whitespace

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -19,7 +19,7 @@ def bubble_sort_by(arr)
     arr.each_with_index do |_x, i|
       left = arr[i]
       if arr[-1] != left
-        arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive? 
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] if yield(arr[i], arr[i + 1]).positive?
       end
     end
   end
